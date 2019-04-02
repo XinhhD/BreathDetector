d4b5b49b8fee965f61ba22c39b12d7a9897986b8
PennaLai <525772737@qq.com>
2019-04-02 17:03:57 +0800

    add graph

--- app/src/main/java/cn/edu/sustc/recoder/Activity/MainActivity.java
+++ app/src/main/java/cn/edu/sustc/recoder/Activity/MainActivity.java
@@ -14,12 +14,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aditya.filebrowser.Constants;
 import com.aditya.filebrowser.FileChooser;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -31,17 +31,17 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.edu.sustc.recoder.Utils.MyRecoder;
 import cn.edu.sustc.recoder.R;
 import cn.edu.sustc.recoder.Utils.Util;
-import cn.edu.sustc.recoder.Utils.XYData;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     Uri selectedFile;
-    TextView chooseFile;
-    Button writeButton;
+    Button startButton;
     Button chooseButton;
     EditText name;
     private String musicPath = "/Lemon.mp3";
@@ -52,32 +52,44 @@ public class MainActivity extends AppCompatActivity {
     // graph data
     LineDataSet dataSet;
     private float graphIndexNow=0;
-    private float interval = 0.001f; // 坐标间隔
-
-
+    public final float MAX_AXIS = 100f;
+    private float interval = 1f; // 坐标间隔
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Util.requestPermission(this, Manifest.permission.RECORD_AUDIO);
         Util.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         setContentView(R.layout.activity_main);
-        writeButton = (Button)findViewById(R.id.write);
-        chooseButton= (Button)findViewById(R.id.choose);
+        startButton = (Button)findViewById(R.id.start);
+        chooseButton = (Button)findViewById(R.id.choose);
         name = (EditText)findViewById(R.id.file);
-        chooseFile = (TextView)findViewById(R.id.filename) ;
-        chart = (LineChart) findViewById(R.id.chart);
+        {
+            // 图表设置
+            chart = (LineChart) findViewById(R.id.chart);
+            XAxis axis = chart.getXAxis();
+            axis.setAxisMaximum(MAX_AXIS);
+            chart.disableScroll(); // 禁止滑动
+//            chart.setBackgroundColor(Color.GREEN);
+            //数据设置
+            List<Entry> entries = new ArrayList<Entry>();
+            Entry e = new Entry(0, 1);
+            entries.add(e);
+            dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+//            dataSet
+            dataSet.setColor(Color.BLACK);
+            dataSet.setValueTextColor(Color.RED); // styling, ...
+            LineData lineData = new LineData(dataSet);
+            chart.setData(lineData);
+            chart.invalidate();
+        }
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("于dd日HH_mm_ss创建");// HH:mm:ss
         Date date = new Date(System.currentTimeMillis());
         name.setText(simpleDateFormat.format(date));
         initMusicFile();
-        setData(makeRandomData());
+        startTimer(); // 定时更新图标
     }
 
     public void onChooseFile(View view){
-        chooseFile();
-    }
-
-    public void chooseFile(){
         if(isPlaying) {
             Toast.makeText(MainActivity.this,"请先停止播放",Toast.LENGTH_SHORT).show();
             return;
@@ -94,21 +106,22 @@ public class MainActivity extends AppCompatActivity {
         if(!isPlaying) {
             record();
             playMusic();
-            writeButton.setText("STOP");
+            startButton.setText("STOP");
             isPlaying = true;
         }else {
             record();
             pauseMusic();
-            writeButton.setText("SYNC");
+            startButton.setText("START");
             isPlaying = false;
         }
-
     }
+
     public void record(){
         if (mc.isRecording) {
             mc.stopRecording();
             Toast.makeText(MainActivity.this,"结束录音",Toast.LENGTH_SHORT).show();
         } else {
+
             String filename = name.getText().toString()+".pcm";
             final File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + Environment.DIRECTORY_MUSIC + File.separator + filename);
             if (!file.mkdirs()) {
@@ -122,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this,"开始录音",Toast.LENGTH_SHORT).show();
         }
     }
+
 
     public void playMusic(){
         mMediaPlayer.start();
@@ -152,40 +166,38 @@ public class MainActivity extends AppCompatActivity {
                 selectedFile = data.getData();
             }
         }
-        chooseFile.setText(selectedFile.toString());
         mMediaPlayer.reset();
         initMusicFile();
     }
 
-    public XYData[] makeRandomData(){
-        XYData[] data = new XYData[100];
-        for (int i=0; i<100; i++){
-            data[i] = new XYData((float)i, (float) Math.random());
-        }
-        return data;
+    public void startTimer(){
+        Timer timer = new Timer();
+        long delay = 1 * 100;
+        long period = 100;
+        timer.schedule(new UpdateTimerTask(), delay, period);
     }
 
-    public void setData(XYData xydata[]){
-        List<Entry> entries = new ArrayList<Entry>();
-        for (XYData data : xydata){
-            entries.add(new Entry(data.getX(), data.getY()));
+    class UpdateTimerTask extends TimerTask{
+        @Override
+        public void run() {
+            updateData((float)Math.random());
+            System.out.println("Update"+graphIndexNow);
         }
-        dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-        dataSet.setColor(Color.BLACK);
-        dataSet.setValueTextColor(Color.RED); // styling, ...
-        LineData lineData = new LineData(dataSet);
-        chart.setData(lineData);
-        chart.invalidate(); // refresh1111111111111111111111111111161
     }
+
 
     public void updateData(float y){
         Entry newData = new Entry(graphIndexNow, y);
+//        dataSet.removeEntryByXValue(graphIndexNow);
+        dataSet.addEntry(newData);
         graphIndexNow += interval;
-        if (graphIndexNow > 10000){  // 超出坐标设成0
+        if (graphIndexNow > MAX_AXIS){
+            dataSet.clear();
             graphIndexNow = 0;
         }
-//        dataSet.removeEntry();
-        dataSet.addEntry(newData);
+        dataSet.notifyDataSetChanged();
+        chart.notifyDataSetChanged();
+        chart.invalidate();
     }
 
 }

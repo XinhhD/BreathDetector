82ae83bb781a4f69afede9fa9841a2eed88129e8
Penna <525772737@qq.com>
2019-05-08 19:44:58 +0800

    加入读取文件

---
+++ app/src/main/java/cn/edu/sustc/recoder/Utils/ReadCsv.java
@@ -0,0 +1,33 @@
+package cn.edu.sustc.recoder.Utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class ReadCsv {
+  public static double[][] ReadCsvFile(String filePath){
+      double[][] result = null;
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(filePath));//换成你的文件名
+      String line = null;
+      ArrayList<String[]> store = new ArrayList<String[]>();
+
+      while((line=reader.readLine())!=null){
+        String item[] = line.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
+        store.add(item);
+      }
+      result = new double[store.get(0).length][store.size()];
+      for(int i=0; i<store.size(); i++){
+        String[] temp = store.get(i);
+        for (int j = 0; j <temp.length ; j++) {
+          result[j][i] = Double.valueOf(temp[j]);
+        }
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return result;
+
+  }
+}
---
+++ app/src/test/java/cn/edu/sustc/recoder/Utils/ReadCsvTest.java
@@ -0,0 +1,13 @@
+package cn.edu.sustc.recoder.Utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReadCsvTest {
+
+  @Test
+  public void readCsvFile() {
+    ReadCsv.ReadCsvFile("D:\\Github\\Penna\\BreathDetector\\app\\src\\main\\java\\cn\\edu\\sustc\\recoder\\Utils\\matrix.csv");
+  }
+}
